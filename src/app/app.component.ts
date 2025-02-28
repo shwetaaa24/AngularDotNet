7085779eb55259d58559560570e715160b6e16ae
Shweta Sawant <shwetas19@birlasoft.com>
2025-02-28 15:29:08 +0530

    initial commit with active-report-js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component ,ViewChild } from '@angular/core';
+import {
+  AR_EXPORTS,
+  HtmlExportService,
+  PdfExportService,
+  ViewerComponent,
+  TabularDataExportService,
+} from '@mescius/activereportsjs-angular';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',providers: [
+    {
+      provide: AR_EXPORTS,
+      useClass: PdfExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: HtmlExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: TabularDataExportService,
+      multi: true,
+    },
+  ],
   standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'active-reporting-app';
+  @ViewChild(ViewerComponent, { static: false }) reportViewer!: ViewerComponent;
+  onViewerInit() {
+    this.reportViewer.open('report.rdlx-json');
+  }
 }
